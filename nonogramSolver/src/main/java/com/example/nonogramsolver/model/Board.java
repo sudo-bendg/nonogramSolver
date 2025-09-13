2ea9f433f9c9eebfa72219b33f889d808e77e75e
Benjamin Godfrey <bendg@disroot.org>
2025-09-13 08:05:22 +0100

    feat: add Board class

---
+++ nonogramSolver/src/main/java/com/example/nonogramsolver/model/Board.java
@@ -0,0 +1,44 @@
+package com.example.nonogramsolver.model;
+
+public class Board {
+  private int xSize;
+  private int ySize;
+  private Cell[][] grid;
+
+  public Board(int xSize, int ySize) {
+    this.xSize = xSize;
+    this.ySize = ySize;
+    this.grid = new Cell[xSize][ySize];
+    
+    for (int i = 0; i < xSize; i++) {
+      for (int j = 0; j < ySize; j++) {
+        this.grid[i][j] = new Cell();
+      }
+    }
+  }
+
+  public void setCell(int x, int y, String value) {
+    this.grid[x][y].setCell(value);
+  }
+
+  public String getCell(int x, int y) {
+    return this.grid[x][y].getCell();
+  }
+
+  public Cell.CellState getCellState(int x, int y) {
+    return this.grid[x][y].getState();
+  }
+
+  public String getGridString() {
+    String returnString = "";
+
+    for (Cell[] row : this.grid) {
+      for (Cell cell : row) {
+        returnString += cell.getCell();
+      }
+      returnString += "\n";
+    }
+
+    return returnString;
+  }
+}
